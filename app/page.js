2d2aa78927a3d3e6c9fe7ca02fc9269a3f64ca02
vicxtunespro <vicitug@gmail.com>
2025-07-17 01:18:01 +0300

    Props

--- app/page.js
+++ app/page.js
@@ -1,31 +1,16 @@
+import ProductCard from "@/components/dementa/ProductCard";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <div className="hero bg-base-200 min-h-screen">
-  <div className="hero-content flex-col lg:flex-row-reverse">
-    <div className="text-center lg:text-left">
-      <h1 className="text-5xl font-bold">Login now!</h1>
-      <p className="py-6">
-        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-        quasi. In deleniti eaque aut repudiandae et a id nisi.
-      </p>
-    </div>
-    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-      <div className="card-body">
-        <fieldset className="fieldset">
-          <label className="label">Email</label>
-          <input type="email" className="input" placeholder="Email" />
-          <label className="label">Password</label>
-          <input type="password" className="input" placeholder="Password" />
-          <div><a className="link link-hover">Forgot password?</a></div>
-          <button className="btn btn-neutral mt-4">Login</button>
-        </fieldset>
-      </div>
-    </div>
-  </div>
-</div>
+      <ProductCard title={"Nike Shoes V1"} desc={"The 1st model of Nike shoes."} />
+      <ProductCard title={"Nike Shoes V2"} desc={"The 2nd model of Nike shoes."} />
+      <ProductCard title={"Nike Shoes V3"} desc={"The 3rd model of Nike shoes."} />
+      <ProductCard title={"Nike Shoes V4"} desc={"The 4th model of Nike shoes."} />
+      <ProductCard title={"Nike Shoes V5"} desc={"The 5th model of Nike shoes."} />
+      <ProductCard title={"Nike Shoes V6"} desc={"The 6th model of Nike shoes."} />
+      <ProductCard title={"Nike Shoes V7"} desc={"The 7th model of Nike shoes."} />
     </div>
   );
 }
